defc9d0cf9273e1d8f4bfe3644086970030c1d5d
vinniejipsk <141219700+vinniejipsk@users.noreply.github.com>
2024-01-22 13:06:57 +0800

    Adjust CreateReviewForm w/ userid

--- daos/reviews.js
+++ daos/reviews.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users',
+    required: true
+  },
   game: {
     type: String,
     required: true,
